4e9e9868eea4f374abf3e57c8a2c968ff9ea85bf
victor <victor.fouquet@gmail.com>
2026-01-04 15:55:23 +0100

    Enhance visitor trait with additionnal methods

--- src/strategies.rs
+++ src/strategies.rs
@@ -22,11 +22,45 @@ pub trait Sampler<Ctx> {
 /// Visitors define custom behavior that executes when a node is encountered.
 /// They can inspect, modify, or collect information from nodes.
 pub trait Visitor<Ctx> {
-    /// Visits a node during traversal.
+    /// Gives the initial search cost when starting the traversal.
+    ///
+    /// Should usually return zero but some business rule may want to override it.
     ///
     /// # Arguments
     ///
-    /// * `node` - The node being visited (mutable access allowed)
+    /// * `node_id` - The start node index
+    /// * `context` - Contextual information available during traversal
+    fn init_cost(&self, node_id: u32, context: &Ctx) -> f64 { 0.0 }
+
+    /// Computes global exploration cost to reach a given node.
+    ///
+    /// Helpful to compute weight accumulation in weighted/heuristic searches
+    ///
+    /// # Arguments
+    ///
+    /// * `from`    - The connection's source node id
+    /// * `to`      - The connection's target node id
+    /// * `context` - Contextual information available during traversal
+    fn exploration_cost(&self, from: u32, to: u32, context: &Ctx) -> f64 { 1.0 }
+
+    /// Determines if a connection should be explored.
+    ///
+    /// Implement to determine if a node is opened or close, if a cheaper path is found...
+    ///
+    /// # Arguments
+    ///
+    /// * `from`    - The connection's source node id
+    /// * `to`      - The connection's target node id
+    /// * `context` - Contextual information available during traversal
+    fn should_explore(&self, from: u32, to: u32, context: &Ctx) -> bool;
+
+    /// Visits a node during traversal.
+    ///
+    /// Implement to keep track of visited nodes, global path, weights propagation...
+    ///
+    /// # Arguments
+    ///
+    /// * `node_id` - The id of the node being visited
     /// * `context` - Contextual information available during traversal
     fn visit(&self, node_id: u32, context: &Ctx);
 }
