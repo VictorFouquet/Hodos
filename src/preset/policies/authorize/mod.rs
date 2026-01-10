2f45a7a6447d0d589f225595d45d05688ba3b185
victor <victor.fouquet@gmail.com>
2026-01-10 18:40:46 +0100

    Add budget authorize preset policy

--- src/preset/policies/authorize/mod.rs
+++ src/preset/policies/authorize/mod.rs
@@ -1,5 +1,8 @@
+pub mod auth_budget;
 pub mod auth_unique;
 pub mod auth_value;
 
+pub use auth_budget::AuthBudget;
 pub use auth_unique::{ UniqueNode, UniqueUnweightedEdge };
 pub use auth_value::{ AllowNodeValue, AllowWeightAbove, AllowWeightUnder };
+
