f4a05d4d1032a1418301d759750b437b06516a81
victor <victor.fouquet@gmail.com>
2026-01-10 18:40:17 +0100

    Implement composite for authorize and terminate policies

--- src/policy/authorize.rs
+++ src/policy/authorize.rs
@@ -1,3 +1,5 @@
+use crate::policy::Composite;
+
 /// A policy for authorizing the addition of entities to the graph.
 ///
 /// Authorization policies decide whether nodes or edges should be accepted
@@ -14,4 +16,17 @@ pub trait Authorize<Entity, Ctx> {
     ///
     /// `true` if the entity should be added, `false` otherwise
     fn add(&mut self, entity: &Entity, context: &Ctx) -> bool;
+}
+
+impl<E, Ctx, P1, P2> Authorize<E, Ctx> for Composite<P1, P2>
+where
+    P1: Authorize<E, Ctx>,
+    P2: Authorize<E, Ctx>,
+{
+    fn add(&mut self, entity: &E, context: &Ctx) -> bool {
+        match self {
+            Composite::And(p1, p2) => p1.add(entity, context) && p2.add(entity, context),
+            Composite::Or(p1, p2) => p1.add(entity, context) || p2.add(entity, context),
+        }
+    }
 }
--- src/policy/terminate.rs
+++ src/policy/terminate.rs
@@ -1,3 +1,5 @@
+use crate::policy::Composite;
+
 /// A policy for determining when graph traversal should terminate.
 ///
 /// Termination policies can be composed to handle target-reached based stop,
@@ -13,4 +15,17 @@ pub trait Terminate {
     ///
     /// `true` if the exploration should stop, `false` otherwise
     fn stop(&self, context: Option<u32>) -> bool;
+}
+
+impl<P1, P2> Terminate for Composite<P1, P2>
+where
+    P1: Terminate,
+    P2: Terminate,
+{
+    fn stop(&self, context: Option<u32>) -> bool {
+        match self {
+            Composite::And(p1, p2) => p1.stop(context) && p2.stop(context),
+            Composite::Or(p1, p2) => p1.stop(context) || p2.stop(context),
+        }
+    }
 }
