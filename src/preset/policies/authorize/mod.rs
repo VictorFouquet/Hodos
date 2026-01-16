f1bc021f90193a4e892f8fac53668ff1fc5f0572
victor <victor.fouquet@gmail.com>
2026-01-16 19:06:39 +0100

    Remove no longer used files

--- src/preset/policies/authorize/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod auth_budget;
-pub mod auth_unique;
-pub mod auth_value;
-
-pub use auth_budget::AuthBudget;
-pub use auth_unique::{ UniqueNode, UniqueEdge };
-pub use auth_value::{ AllowNodeValue, AllowWeightAbove, AllowWeightUnder };
-
--- tests/common/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod edge_mock;
-pub mod node_mock;
-pub mod policy_mock;
-pub mod strategy_mock;
-
-//pub use node_mock::MockNode;
-//pub use edge_mock::MockEdge;
