21c1524d7cf3cff9762fe45111243f25c05aec5c
victor <victor.fouquet@gmail.com>
2026-01-21 00:28:20 +0100

    Add graph integration tests - Compose DFS

--- tests/graph_integration.rs
+++ tests/graph_integration.rs
@@ -69,6 +69,18 @@ mod graph_integration {
         }
 
         #[test]
+        fn handles_unreachable_goal() {
+            let context = vec![vec![1], vec![2], vec![], vec![4], vec![]];
+            let visitor = run_bfs(4, context);
+
+            assert_eq!(visitor.get_parent(4), None);
+
+            assert_eq!(visitor.get_parent(2), Some(1));
+            assert_eq!(visitor.get_parent(1), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+        }
+
+        #[test]
         fn finds_shortest_path() {
             let goal = 4;
             let context = vec![vec![1], vec![0, 2, 3], vec![2, 3], vec![4], vec![3]];
@@ -79,25 +91,98 @@ mod graph_integration {
             assert_eq!(visitor.get_parent(1), Some(0));
             assert_eq!(visitor.get_parent(0), None);
         }
+    }
 
-        // #[test]
-        // fn finds_shortest_path_with_bfs() {
-        //     let goal = 4;
-        //     let context = vec![
-        //         vec![(1, 5.0)],
-        //         vec![(0, 3.0), (2, 11.0), (3, 8.0)],
-        //         vec![(2, 7.0), (3, 1.0)],
-        //         vec![(4, 8.0)],
-        //         vec![(4, 14.0)],
-        //     ];
-        //     let mut visitor = WeightedVisitor::new(GoalReached::new(goal));
-        //     GraphBuilder::new(
-        //         Composite::And(DenyDanglingEdge::default(), AllowWeightBelow::new(10.0)),
-        //         AllowAll::default(),
-        //         WeightedAdjacencySampler::new(),
-        //     )
-        //     .build(&context)
-        //     .traverse(0, &mut MinHeap::new(), &mut visitor);
-        // }
+    mod dfs {
+        use super::*;
+        use hodos::frontier::Stack;
+        use hodos::preset::policies::structural::DenyDanglingEdge;
+        use hodos::preset::policies::traversal::GoalReached;
+        use hodos::preset::policies::value::AllowAll;
+        use hodos::preset::samplers::SimpleAdjacencySampler;
+
+        fn run_dfs(goal: u32, context: Vec<Vec<u32>>) -> SimpleVisitor<GoalReached> {
+            let mut visitor = SimpleVisitor::new(GoalReached::new(goal));
+            GraphBuilder::new(
+                DenyDanglingEdge::default(),
+                AllowAll::default(),
+                SimpleAdjacencySampler::new(),
+            )
+            .build(&context)
+            .traverse(0, &mut Stack::new(), &mut visitor);
+
+            visitor
+        }
+
+        #[test]
+        fn solves_multiple_path() {
+            let context = vec![
+                vec![1, 2, 3],
+                vec![4],
+                vec![5],
+                vec![6],
+                vec![7],
+                vec![7],
+                vec![7],
+                vec![],
+            ];
+            let visitor = run_dfs(7, context);
+
+            assert_eq!(visitor.get_parent(7), Some(6));
+            assert_eq!(visitor.get_parent(6), Some(3));
+            assert_eq!(visitor.get_parent(3), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+
+            assert_eq!(visitor.get_parent(4), None);
+            assert_eq!(visitor.get_parent(5), None);
+        }
+
+        #[test]
+        fn solves_linear_graph() {
+            let context = vec![vec![1], vec![2], vec![3], vec![4], vec![5], vec![]];
+            let visitor = run_dfs(5, context);
+
+            assert_eq!(visitor.get_parent(5), Some(4));
+            assert_eq!(visitor.get_parent(4), Some(3));
+            assert_eq!(visitor.get_parent(3), Some(2));
+            assert_eq!(visitor.get_parent(2), Some(1));
+            assert_eq!(visitor.get_parent(1), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+        }
+
+        #[test]
+        fn solves_cyclic_graph() {
+            let context = vec![vec![1], vec![2], vec![3, 0], vec![4], vec![]];
+            let visitor = run_dfs(4, context);
+
+            assert_eq!(visitor.get_parent(4), Some(3));
+            assert_eq!(visitor.get_parent(3), Some(2));
+            assert_eq!(visitor.get_parent(2), Some(1));
+            assert_eq!(visitor.get_parent(1), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+        }
+
+        #[test]
+        fn solves_disconnected_graph() {
+            let context = vec![vec![1], vec![2], vec![], vec![4], vec![]];
+            let visitor = run_dfs(2, context);
+
+            assert_eq!(visitor.get_parent(2), Some(1));
+            assert_eq!(visitor.get_parent(1), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+
+            assert_eq!(visitor.get_parent(4), None);
+        }
+
+        #[test]
+        fn handles_unreachable_goal() {
+            let context = vec![vec![1], vec![2], vec![], vec![4], vec![]];
+            let visitor = run_dfs(4, context);
+
+            assert_eq!(visitor.get_parent(4), None);
+            assert_eq!(visitor.get_parent(2), Some(1));
+            assert_eq!(visitor.get_parent(1), Some(0));
+            assert_eq!(visitor.get_parent(0), None);
+        }
     }
 }
