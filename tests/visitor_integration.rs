79b628526e12c495dc949b940a85db3bd0db223a
victor <victor.fouquet@gmail.com>
2026-01-20 13:51:22 +0100

    Add integretion tests for weighted visitor and terminate policies

--- tests/visitor_integration.rs
+++ tests/visitor_integration.rs
@@ -8,43 +8,96 @@ mod visitor_integration {
         use hodos::preset::policies::traversal::GoalReached;
         use hodos::preset::policies::traversal::OpeningExhausted;
 
-        #[test]
-        fn stops_when_goal_reached() {
-            let goal = 2;
-            let visitor = SimpleVisitor::new(GoalReached::new(goal));
+        mod simple_visitor {
+            use super::*;
 
-            assert!(!visitor.should_stop(0, &()));
-            assert!(!visitor.should_stop(1, &()));
-            assert!(visitor.should_stop(goal, &()));
+            #[test]
+            fn stops_when_goal_reached() {
+                let goal = 2;
+                let visitor = SimpleVisitor::new(GoalReached::new(goal));
+
+                assert!(!visitor.should_stop(0, &()));
+                assert!(!visitor.should_stop(1, &()));
+                assert!(visitor.should_stop(goal, &()));
+            }
+
+            #[test]
+            fn stops_when_budget_opening_exhausted() {
+                let mut visitor = SimpleVisitor::new(OpeningExhausted::new(2));
+
+                assert!(!visitor.should_stop(0, &()));
+                visitor.visit(0, &());
+
+                assert!(!visitor.should_stop(1, &()));
+                visitor.visit(1, &());
+
+                assert!(visitor.should_stop(2, &()));
+            }
+
+            #[test]
+            fn stops_when_goal_reached_or_opening_exhausted() {
+                let goal = 3;
+                let policy = Composite::Or(GoalReached::new(goal), OpeningExhausted::new(1));
+
+                let mut visitor = SimpleVisitor::new(policy);
+
+                assert!(!visitor.should_stop(0, &())); // Rejects if not goal and budget respected
+
+                visitor.visit(0, &());
+
+                assert!(visitor.should_stop(0, &())); // Complies for budget exhausted
+
+                assert!(visitor.should_stop(goal, &())); // Complies for goal reached
+            }
         }
 
-        #[test]
-        fn stops_when_budget_opening_exhausted() {
-            let mut visitor = SimpleVisitor::new(OpeningExhausted::new(2));
+        mod weighted_visitor {
+            use super::*;
+            use hodos::graph::*;
+            use hodos::preset::{EmptyNode, WeightedEdge};
 
-            assert!(!visitor.should_stop(0, &()));
-            visitor.visit(0, &());
+            fn get_graph() -> Graph<EmptyNode, WeightedEdge> {
+                Graph::<EmptyNode, WeightedEdge>::new()
+            }
 
-            assert!(!visitor.should_stop(1, &()));
-            visitor.visit(1, &());
+            #[test]
+            fn stops_when_goal_reached() {
+                let goal = 2;
+                let visitor = WeightedVisitor::new(GoalReached::new(goal));
 
-            assert!(visitor.should_stop(2, &()));
-        }
+                assert!(!visitor.should_stop(0, &get_graph()));
+                assert!(!visitor.should_stop(1, &get_graph()));
+                assert!(visitor.should_stop(goal, &get_graph()));
+            }
 
-        #[test]
-        fn stops_when_goal_reached_or_opening_exhausted() {
-            let goal = 3;
-            let policy = Composite::Or(GoalReached::new(goal), OpeningExhausted::new(1));
+            #[test]
+            fn stops_when_budget_opening_exhausted() {
+                let mut visitor = WeightedVisitor::new(OpeningExhausted::new(2));
 
-            let mut visitor = SimpleVisitor::new(policy);
+                assert!(!visitor.should_stop(0, &get_graph()));
+                visitor.visit(0, &get_graph());
 
-            assert!(!visitor.should_stop(0, &())); // Rejects if not goal and budget respected
+                assert!(!visitor.should_stop(1, &get_graph()));
+                visitor.visit(1, &get_graph());
 
-            visitor.visit(0, &());
+                assert!(visitor.should_stop(2, &get_graph()));
+            }
 
-            assert!(visitor.should_stop(0, &())); // Complies for budget exhausted
+            #[test]
+            fn stops_when_goal_reached_or_opening_exhausted() {
+                let goal = 3;
+                let policy = Composite::Or(GoalReached::new(goal), OpeningExhausted::new(1));
 
-            assert!(visitor.should_stop(goal, &())); // Complies for goal reached
+                let mut visitor = WeightedVisitor::new(policy);
+
+                assert!(!visitor.should_stop(0, &get_graph())); // Rejects if not goal and budget respected
+
+                visitor.visit(0, &get_graph());
+
+                assert!(visitor.should_stop(0, &get_graph())); // Complies for budget exhausted
+
+                assert!(visitor.should_stop(goal, &get_graph())); // Complies for goal reached
+            }
         }
     }
 }
