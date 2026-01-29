86c10811e933ff0a70b4ec4294d31592b3654c47
victor <victor.fouquet@gmail.com>
2026-01-29 08:56:42 +0100

    Add Frontier mod to core

---
+++ src/core/frontier/mod.rs
@@ -0,0 +1,32 @@
+/// A strategy for managing which nodes to explore next during graph traversal.
+///
+/// Frontiers determine the order in which nodes are visited. Different implementations
+/// can be used to match predefined search algorith (BFS, DFS, Dijkstra...)
+pub trait Frontier {
+    /// Creates a new empty frontier.
+    fn new() -> Self
+    where
+        Self: Sized;
+
+    /// Adds a node to the frontier.
+    ///
+    /// # Arguments
+    ///
+    /// * `id`   - Id of the node to add
+    /// * `cost` - Optional cost to handle priority
+    fn push(&mut self, id: u32, cost: Option<f64>);
+
+    /// Removes and returns the next node ID to visit.
+    ///
+    /// # Returns
+    ///
+    /// `Some(node_id)` if nodes remain, `None` if frontier is empty
+    fn pop(&mut self) -> Option<u32>;
+
+    /// Checks if the frontier is empty.
+    ///
+    /// # Returns
+    ///
+    /// `true` if no nodes remain, `false` otherwise
+    fn is_empty(&self) -> bool;
+}
