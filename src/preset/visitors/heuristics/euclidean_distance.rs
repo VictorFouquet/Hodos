d27f01e8ad1040c9095bd04a219d8d26b65edf03
victor <victor.fouquet@gmail.com>
2026-02-01 12:36:56 +0100

    Add euclidean distance estimator

---
+++ src/preset/visitors/heuristics/euclidean_distance.rs
@@ -0,0 +1,142 @@
+use crate::core::{Edge, Graph, HasData, HasPosition, HeuristicEstimator, Node};
+
+#[derive(Debug)]
+pub struct EuclideanDistance {
+    target_x: f64,
+    target_y: f64,
+}
+
+impl EuclideanDistance {
+    pub fn new(target_x: f64, target_y: f64) -> Self {
+        EuclideanDistance { target_x, target_y }
+    }
+}
+
+impl<N, E, T> HeuristicEstimator<N, E> for EuclideanDistance
+where
+    N: Node + HasData<Data = T>,
+    E: Edge,
+    T: HasPosition,
+{
+    fn heuristic(&self, node_id: u32, graph: &Graph<N, E>) -> f64 {
+        let node = graph.get_node(node_id).unwrap();
+        let data = node.data();
+        let dx = data.x() - self.target_x;
+        let dy = data.y() - self.target_y;
+        (dx * dx + dy * dy).sqrt()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        core::{Edge, Graph, HasData, HasPosition, HeuristicEstimator, Node},
+        preset::visitors::EuclideanDistance,
+    };
+
+    #[test]
+    fn computes_euclidean_distance() {
+        let mut graph = Graph::<MockNode, MockEdge>::new();
+        let estimator = EuclideanDistance::new(2.0, 2.0);
+
+        graph.add_node(MockNode {
+            id: 0,
+            data: Point { x: 0.0, y: 0.0 },
+        });
+        graph.add_node(MockNode {
+            id: 1,
+            data: Point { x: 0.0, y: 1.0 },
+        });
+        graph.add_node(MockNode {
+            id: 2,
+            data: Point { x: 1.0, y: 1.0 },
+        });
+        graph.add_node(MockNode {
+            id: 3,
+            data: Point { x: 1.0, y: 2.0 },
+        });
+        graph.add_node(MockNode {
+            id: 4,
+            data: Point { x: 2.0, y: 2.0 },
+        });
+
+        assert_eq!(round2(estimator.heuristic(0, &graph)), 2.82);
+        assert_eq!(round2(estimator.heuristic(1, &graph)), 2.23);
+        assert_eq!(round2(estimator.heuristic(2, &graph)), 1.41);
+        assert_eq!(round2(estimator.heuristic(3, &graph)), 1.0);
+        assert_eq!(round2(estimator.heuristic(4, &graph)), 0.0);
+    }
+
+    #[test]
+    fn euclidean_distance_handles_negative_coordinates() {
+        let mut graph = Graph::<MockNode, MockEdge>::new();
+        let estimator = EuclideanDistance::new(0.0, 0.0);
+
+        graph.add_node(MockNode {
+            id: 0,
+            data: Point { x: 0.0, y: 0.0 },
+        });
+        graph.add_node(MockNode {
+            id: 1,
+            data: Point { x: 0.0, y: -1.0 },
+        });
+        graph.add_node(MockNode {
+            id: 2,
+            data: Point { x: -1.0, y: 0.0 },
+        });
+        graph.add_node(MockNode {
+            id: 3,
+            data: Point { x: -1.0, y: -1.0 },
+        });
+        graph.add_node(MockNode {
+            id: 4,
+            data: Point { x: -2.0, y: -2.0 },
+        });
+
+        assert_eq!(round2(estimator.heuristic(0, &graph)), 0.0);
+        assert_eq!(round2(estimator.heuristic(1, &graph)), 1.0);
+        assert_eq!(round2(estimator.heuristic(2, &graph)), 1.0);
+        assert_eq!(round2(estimator.heuristic(3, &graph)), 1.41);
+        assert_eq!(round2(estimator.heuristic(4, &graph)), 2.82);
+    }
+
+    struct MockEdge;
+    impl Edge for MockEdge {}
+
+    struct MockNode {
+        id: u32,
+        data: Point,
+    }
+
+    impl Node for MockNode {
+        fn id(&self) -> u32 {
+            self.id
+        }
+    }
+
+    impl HasData for MockNode {
+        type Data = Point;
+        fn data(&self) -> &Self::Data {
+            &self.data
+        }
+    }
+
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    impl HasPosition for Point {
+        fn x(&self) -> f64 {
+            self.x
+        }
+
+        fn y(&self) -> f64 {
+            self.y
+        }
+    }
+
+    fn round2(x: f64) -> f64 {
+        (x * 100.0).floor() / 100.0
+    }
+}
--- src/preset/visitors/heuristics/mod.rs
+++ src/preset/visitors/heuristics/mod.rs
@@ -1,5 +1,7 @@
+pub mod euclidean_distance;
 pub mod manhattan_distance;
 pub mod zero_heuristic;
 
+pub use euclidean_distance::EuclideanDistance;
 pub use manhattan_distance::ManhattanDistance;
 pub use zero_heuristic::ZeroHeuristic;
